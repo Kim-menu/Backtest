63b7b3701902bed99ba6d4a7ea0201ce03e33cc1
Kim-menu <hyunjin@gmail.com>
2022-01-02 16:06:06 +0900

    Basic Backtest program added with some data

---
+++ main.py
@@ -0,0 +1,13 @@
+import BackTester
+
+
+# TQQQ experiment
+#my_back_tester = BackTester.SmurfTester(0)
+#my_back_tester = BackTester.SimpleTester(0)
+#my_back_tester.simulation('TQQQ', '2012-01-01', '2021-12-31', 1000)
+
+
+# SAMSUNG experiment
+#my_back_tester = BackTester.SmurfTester(0)
+#my_back_tester = BackTester.SimpleTester(0)
+#my_back_tester.simulation('005935', '2012-01-01', '2021-12-31', 500000)
