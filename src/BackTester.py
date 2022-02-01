944a18f10021d23d0e7f2fe59908b0179c337c38
Kim-menu <hyunjin@gmail.com>
2022-02-02 02:46:58 +0900

    Backtest rebalancing strategy
    - Implement RebalancingTester class

--- src/BackTester.py
+++ src/BackTester.py
@@ -36,6 +36,13 @@ class BackTester(metaclass=ABCMeta):
         self.withdraw(price * buy_share)
         self.share += buy_share
 
+    def sell(self, ticker_data, today, sell_share):
+        str_date = util.date_to_str(today)
+        price = ticker_data['Close'][str_date]
+        sell_share = min(sell_share, self.share)
+        self.share -= sell_share
+        self.deposit(price * sell_share)
+
     def valid(self, ticker_data, today):
         str_date = util.date_to_str(today)
         try:
@@ -46,15 +53,18 @@ class BackTester(metaclass=ABCMeta):
             self.last_price = close
             return True
 
+    def get_eval(self, today):
+        return round((self.balance + self.share * self.last_price), 2)
+
     def status(self, today):
         print(util.date_to_str(today),
               "|", round(self.last_price, 2),
               "|", round(self.balance, 2),
               "|", self.share,
-              "|", round(self.balance + self.share * self.last_price, 2))
+              "|", self.get_eval(today))
 
     @abstractmethod
-    def decide_to_buy(self, ticker_data, today):
+    def trade(self, ticker_data, today):
         pass
 
     @abstractmethod
@@ -64,7 +74,7 @@ class BackTester(metaclass=ABCMeta):
 
 # Smurf strategy
 class SmurfTester(BackTester):
-    def decide_to_buy(self, ticker_data, today):
+    def trade(self, ticker_data, today):
         if self.valid(ticker_data, today):
             str_date = util.date_to_str(today)
             change = ticker_data['Change'][str_date]
@@ -81,7 +91,7 @@ class SmurfTester(BackTester):
         print("date| price| balance| share| eval")
         while today <= end_date:
             self.period_check(today, monthly_money)
-            if self.decide_to_buy(ticker_data, today):
+            if self.trade(ticker_data, today):
                 if today.day > 24:
                     self.buy(ticker_data, today, 10000)
                 else:
@@ -95,7 +105,7 @@ class SmurfTester(BackTester):
 class SimpleTester(BackTester):
     this_month_buy = False
 
-    def decide_to_buy(self, ticker_data, today):
+    def trade(self, ticker_data, today):
         if self.valid(ticker_data, today):
             if not self.this_month_buy:
                 return True
@@ -112,9 +122,41 @@ class SimpleTester(BackTester):
             if util.is_month_start(today):
                 self.this_month_buy = False
             self.period_check(today, monthly_money)
-            if self.decide_to_buy(ticker_data, today):
+            if self.trade(ticker_data, today):
                 self.buy(ticker_data, today, 10000)
                 self.this_month_buy = True
             if self.valid(ticker_data, today):
                 self.status(today)
             today = util.day_pass(today)
+
+
+# Rebalancing strategy
+class RebalancingTester(BackTester):
+    def trade(self, ticker_data, today):
+        if self.valid(ticker_data, today):
+            if self.risk_ratio(today) < 0.2 or self.risk_ratio(today) > 0.4:
+                return True
+        return False
+
+    def risk_ratio(self, today):
+        return self.balance / self.get_eval(today)
+
+    def simulation(self, str_ticker, str_start_date, str_end_date, monthly_money):
+        ticker_data = Fdr.DataReader(str_ticker,
+                                     str_start_date,
+                                     str_end_date)
+        today = util.str_to_date(str_start_date)
+        end_date = util.str_to_date(str_end_date)
+        print("date| price| balance| share| eval")
+        while today <= end_date:
+            self.period_check(today, monthly_money)
+            if self.trade(ticker_data, today):
+                self.rebalancing(ticker_data, today)
+            if self.valid(ticker_data, today):
+                self.status(today)
+            today = util.day_pass(today)
+
+    def rebalancing(self, ticker_data, today):
+        target_share = (self.get_eval(today) * 0.7) // self.last_price
+        self.sell(ticker_data, today, 10000)
+        self.buy(ticker_data, today, target_share)
--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
 import BackTester
 
 
-# TQQQ experiment
-my_back_tester = BackTester.SmurfTester(0)
-#my_back_tester = BackTester.SimpleTester(0)
-my_back_tester.simulation('TQQQ', '2012-01-01', '2021-12-31', 1000)
+# my_back_tester = BackTester.SmurfTester(0)
+# my_back_tester = BackTester.SimpleTester(0)
+my_back_tester = BackTester.RebalancingTester(0)
 
+# TQQQ experiment
+my_back_tester.simulation('TQQQ', '2021-01-01', '2021-12-31', 800)
 
 # SAMSUNG experiment
-#my_back_tester = BackTester.SmurfTester(0)
-#my_back_tester = BackTester.SimpleTester(0)
-#my_back_tester.simulation('005935', '2012-01-01', '2021-12-31', 500000)
+# my_back_tester.simulation('005935', '2021-01-01', '2021-12-31', 500000)
